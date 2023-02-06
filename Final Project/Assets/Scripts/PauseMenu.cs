ae1915db93956eb15b2773e3440ac4d575c24cff
KenjiIlao <wendyroselane@gmail.com>
2023-02-06 21:35:16 +0800

    PAUSE

--- Final Project/Assets/Scripts/PauseMenu.cs
+++ Final Project/Assets/Scripts/PauseMenu.cs
@@ -5,49 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 { 
-    public GameObject pauseMenu;
+  [SerializeField] GameObject pauseMenu;
 
-    public static bool isPaused;
+  public void Pause()
+  {
+    pauseMenu.SetActive(true);
+    Time.timeScale = 0f;
+  }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        pauseMenu.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            if(isPaused)
-            {
-                ResumeGame();
-            }
-            else
-            {
-                PauseGame();
-            }
-        }
-    }
-
-    public void PauseGame()
-    {
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0f;
-        isPaused = true;
-    }
-
-    public void ResumeGame()
-    {
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
-        isPaused = false;
-    }
-
-    public void GoToMainMenu()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("Menu");
-    }
+  public void Resume()
+  {
+    pauseMenu.SetActive(false);
+    Time.timeScale = 1f;
+  }
+  
 }
